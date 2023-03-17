6cb62d95bf1ee8458e8fd623b07afc67dce61b4e
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-03-16 21:58:57 -0400

    bigger messages

--- MyOpenAI/ChatUI.py
+++ MyOpenAI/ChatUI.py
@@ -1,54 +1,62 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QTextEdit, QLineEdit, QPushButton, QScrollArea, QLabel
 from .Conversation import Conversation, Message
+from PyQt5.QtWidgets import QListWidget, QListWidgetItem, QFrame
+from PyQt5.QtCore import Qt
+
+class MessageView(QFrame):
+	def __init__(self, message: Message):
+		super().__init__()
+
+		self.layout = QVBoxLayout()
+		self.setLayout(self.layout)
+
+		self.label = QLabel()
+		self.label.setText(f"{message['role']}: {message['content']}")
+		self.label.setWordWrap(True)
+		self.layout.addWidget(self.label)
 
 class ChatUI(QWidget):
 	def __init__(self, conversation: Conversation):
 		super().__init__()
 
-		self.setWindowTitle('Chatbot')
-		self.setGeometry(100, 100, 600, 400)
-
-		main_layout = QVBoxLayout()
-
-		self.chat_log = QVBoxLayout()
-
-		self.scroll_area = QScrollArea()
-		scroll_widget = QWidget()
-		scroll_widget.setLayout(self.chat_log)
-		self.scroll_area.setWidget(scroll_widget)
-		self.scroll_area.setWidgetResizable(True)
-
-		main_layout.addWidget(self.scroll_area)
-
-		input_layout = QHBoxLayout()
-
-		self.input_box = QLineEdit()
-		input_layout.addWidget(self.input_box)
-
-		send_button = QPushButton('Send')
-		send_button.clicked.connect(self.send_message)
-		input_layout.addWidget(send_button)
-
-		main_layout.addLayout(input_layout)
-		self.setLayout(main_layout)
-		
 		self.conversation = conversation
-		for message in self.conversation.messages:
-			self.add_message(message["role"], message.json.get("name","name N/A"), message["content"])
+		self.init_ui()
+		for message in conversation.messages:
+			self.add_message(message)
+
+	def init_ui(self):
+		self.setWindowTitle('Chat')
+
+		self.layout = QVBoxLayout()
+		self.setLayout(self.layout)
+
+		self.list_view = QListWidget()
+		self.layout.addWidget(self.list_view)
+
+		self.input_field = QLineEdit()
+		self.input_field.setPlaceholderText("Type your message here...")
+		self.input_field.returnPressed.connect(self.send_message)
+		self.layout.addWidget(self.input_field)
 
 	def send_message(self):
-		message = self.input_box.text().strip()
-		if message:
-			self.add_message('user', 'User', message)
-			self.input_box.clear()
-			response = self.get_chatbot_response(message)
-			self.add_message('assistant', 'Chatbot', response)
+		message_text = self.input_field.text().strip()
+		if message_text:
+			message = Message.from_role_content("user", message_text)
+			self.add_message(message)
+			self.input_field.clear()
 
-	def add_message(self, role, name, message):
-		message_label = QLabel(f'<b>{role.capitalize()} ({name}):</b> {message}')
-		self.chat_log.addWidget(message_label)
+			# Handle response from the chat bot and render the assistant's message
+			# Here's a placeholder example:
+			response_text = "Assistant's response to: " + message_text
+			response = Message.from_role_content("assistant", response_text)
+			self.add_message(response)
 
-	def get_chatbot_response(self, message):
-		# Replace this function with your actual chatbot function
-		return "This is the AI response."
+	def add_message(self, message: Message):
+		# self.conversation.add_message(message)
+		item = QListWidgetItem()
+		item_widget = MessageView(message)
+		item.setSizeHint(item_widget.sizeHint())
+		self.list_view.addItem(item)
+		self.list_view.setItemWidget(item, item_widget)
+		self.list_view.scrollToBottom()
