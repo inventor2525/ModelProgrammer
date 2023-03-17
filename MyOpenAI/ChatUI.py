47a1173a7b7fb89373c3dcf055d98791ee32ec63
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-03-16 21:00:03 -0400

    wip ui

---
+++ MyOpenAI/ChatUI.py
@@ -0,0 +1,58 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QTextEdit, QLineEdit, QPushButton, QScrollArea, QLabel
+
+class ChatUI(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Chatbot')
+        self.setGeometry(100, 100, 600, 400)
+
+        main_layout = QVBoxLayout()
+
+        self.chat_log = QVBoxLayout()
+
+        self.scroll_area = QScrollArea()
+        scroll_widget = QWidget()
+        scroll_widget.setLayout(self.chat_log)
+        self.scroll_area.setWidget(scroll_widget)
+        self.scroll_area.setWidgetResizable(True)
+
+        main_layout.addWidget(self.scroll_area)
+
+        input_layout = QHBoxLayout()
+
+        self.input_box = QLineEdit()
+        input_layout.addWidget(self.input_box)
+
+        send_button = QPushButton('Send')
+        send_button.clicked.connect(self.send_message)
+        input_layout.addWidget(send_button)
+
+        main_layout.addLayout(input_layout)
+        self.setLayout(main_layout)
+
+    def send_message(self):
+        message = self.input_box.text().strip()
+        if message:
+            self.add_message('user', 'User', message)
+            self.input_box.clear()
+            response = self.get_chatbot_response(message)
+            self.add_message('assistant', 'Chatbot', response)
+
+    def add_message(self, role, name, message):
+        message_label = QLabel(f'<b>{role.capitalize()} ({name}):</b> {message}')
+        self.chat_log.addWidget(message_label)
+
+    def get_chatbot_response(self, message):
+        # Replace this function with your actual chatbot function
+        return "This is the AI response."
+
+def main():
+    app = QApplication(sys.argv)
+    chat_ui = ChatUI()
+    chat_ui.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
