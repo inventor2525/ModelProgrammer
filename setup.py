8609938974967bd2f7a0b25781fb4ff324ed64e7
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-04-02 15:05:53 -0400

    fixed setup

--- setup.py
+++ setup.py
@@ -5,21 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ModelProgrammer",
-    version="0.0.1",
+    version="0.0.2",
     author="Charlie Angela Mehlenbeck",
     author_email="charlie_inventor2003@yahoo.com",
     description="An experiment in AI terminal interaction.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/my-openai-project",
+    url="https://github.com/inventor2525/ModelProgrammer",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GPL-3.0 License",
-        "Operating System :: Linux",
-        "Programming Language :: Python :: 3.10",
-    ],
     python_requires=">=3.10",
     install_requires=[
         "tiktoken",
