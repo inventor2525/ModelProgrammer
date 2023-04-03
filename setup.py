bd49ba954626da5aabd04b8be1f3880085612035
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-04-02 20:14:20 -0400

    version change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ModelProgrammer",
-    version="0.0.2",
+    version="0.0.3",
     author="Charlie Angela Mehlenbeck",
     author_email="charlie_inventor2003@yahoo.com",
     description="An experiment in AI terminal interaction.",
