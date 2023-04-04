b2e5de37fafada98007eee0502ae11e6f50bf9d5
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-04-03 23:07:10 -0400

    Revert "Merge branch 'database_improvements'"
    This reverts commit 54f764a161f8e62284a0d24605fc1bc53b3edea0, reversing
    changes made to bd49ba954626da5aabd04b8be1f3880085612035.

--- main.py
+++ main.py
@@ -8,19 +8,15 @@ if __name__ == '__main__':
 	os.makedirs(os.path.expanduser("~/.config/ModelProgrammer"), exist_ok=True)
 	
 	# Check if the database exists, if not, load the demo database if it exists:
-	database_version = '1'
+	database_version = '0'
 	database_path = os.path.expanduser(f"~/.config/ModelProgrammer/Programmer_v{database_version}.db")
 	if os.path.exists(database_path):
 		print(f"Loading user database at {database_path}.")
-		db = ConversationDB(database_path)
 	else:
 		print(f"v{database_version} user database not found.")
-		old_db_path = os.path.expanduser("~/.config/ModelProgrammer/Programmer_v0.db")
-		if os.path.exists(old_db_path):
-			print("Migrating old database. (This may take a while)")
-			db = ConversationDB(database_path)
-			db.migrate_data(old_db_path)
-		elif os.path.exists("demo.db"):
+		print("Note: Time is not yet being spent on database migration, so if you have an older database it will still be in there, but will not be loaded.")
+		
+		if os.path.exists("demo.db"):
 			print("Loading demo database.")
 			
 			# Copy the demo database to the user's database_path:
@@ -28,10 +24,10 @@ if __name__ == '__main__':
 				data = f.read()
 			with open(database_path, 'wb') as f:
 				f.write(data)
-			db = ConversationDB(database_path)
 		else:
 			print("Demo database not found either. Creating new database.")
 			db = ConversationDB(database_path)
+	db = ConversationDB(database_path)
 	
 	# Load the latest conversation:
 	conversation = db.load_conversation(db.latest_conversation)
